845a70dca16bf7bc96b8e2b8255d4b02359fc4d9
Kevin <kdoherty@mit.edu>
2022-05-02 14:06:51 -0400

    First pass at setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='mac',
+version='0.1',
+description='Fast algebraic connectivity maximization',
+url='#',
+author='keevindoherty',
+install_requires=['numpy', 'networkx'],
+author_email='kdoherty@mit.edu',
+packages=setuptools.find_packages(),
+zip_safe=False)
