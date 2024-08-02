7e21f939d21594e86f6b1418619a5f689273001e
Kevin <kdoherty@mit.edu>
2024-08-02 18:39:49 -0400

    Add test for complete graph.

--- tests/utils/test_fiedler.py
+++ tests/utils/test_fiedler.py
@@ -8,12 +8,29 @@ Author: Kevin Doherty
 
 import unittest
 import numpy as np
+import networkx as nx
 
-import mac.utils.fiedler import *
+from mac.utils.conversions import nx_to_mac
+from mac.utils.graphs import weight_graph_lap_from_edge_list
+
+# Code under test.
+from mac.utils.fiedler import *
+
+from networkx.linalg.algebraicconnectivity import algebraic_connectivity
 
 class TestFiedlerUtils(unittest.TestCase):
     def setUp(self):
-        return
+        # Complete graph on 5 nodes
+        self.complete_graph = nx.complete_graph(5)
+
+    def test_complete_graph(self):
+        # The algebraic connectivity of the complete graph K(N) on N nodes is
+        # exactly equal to N.
+        edge_list = nx_to_mac(self.complete_graph)
+        N = self.complete_graph.number_of_nodes()
+        L = weight_graph_lap_from_edge_list(edge_list, N)
+        fiedler_value, fiedler_vec, _ = find_fiedler_pair(L)
+        self.assertTrue(np.isclose(fiedler_value, N))
 
     def test_disconnected(self):
         # Test multiple connected components
