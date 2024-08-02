a28bd8d910a322d8fabc11adca753f9e7abbde61
Kevin <kdoherty@mit.edu>
2024-08-02 16:14:49 -0400

    Test structure now mirrors project structure.

---
+++ tests/utils/test_fiedler.py
@@ -0,0 +1,24 @@
+"""
+Copyright 2023 MIT Marine Robotics Group
+
+Tests for Fiedler utilities
+
+Author: Kevin Doherty
+"""
+
+import unittest
+import numpy as np
+
+import mac.fiedler as fiedler
+from .utils import get_split_petersen_graph
+
+class TestFiedler(unittest.TestCase):
+    def setUp(self):
+        return
+
+    def test_cache(self):
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
